85e96f47dd5c03b81b163bc26ff20aa372f5aab8
alex-cheb <51723582+alex-cheb@users.noreply.github.com>
2019-06-16 17:00:17 +0300

    Update searchTest.java
    Altered search queries to add case of an existing note

--- src/main/java/searchTest.java
+++ src/main/java/searchTest.java
@@ -1,5 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import objects.Anotepad;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,18 +11,22 @@ public class searchTest {
     WebDriver  driver;
     Anotepad np;
 
+    //Not existing note
     String validSearch = "Note";
+    //Search that crashes app
     String injectionSearch = "'><a>";
+    //Existing Note
+    String existingNote = "title";
     @Before
     public void openBrowser() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         np = new Anotepad(driver);
     }
-    @After
+    /*@After
     public void closeBrowser(){
         driver.quit();
-    }
+    }*/
     @Test
     public void searchingTest(){
         np.openLoginPage().login("2@e.ee","2");
@@ -31,6 +36,9 @@ public class searchTest {
         np.openHomePage();
         np.addSearchQuery(injectionSearch).startSearch();
         Assert.assertEquals(np.errorMsg(), np.errorPage());
+        np.openHomePage();
+        np.addSearchQuery(existingNote).startSearch();
+        Assert.assertEquals(np.existingNoteConten(), np.searchNote());
 
 
     }
